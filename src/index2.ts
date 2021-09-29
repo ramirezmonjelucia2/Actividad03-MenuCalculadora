554c7a764d09184c1ef66032dc08151a124f3165
Ramirez Monje Lucia <lrammon392@g.educaand.es>
2021-09-29 08:46:13 +0200

    first commit

---
+++ src/index2.ts
@@ -0,0 +1,28 @@
+import { leerTeclado } from './util/entradaTeclado'
+
+let main = async () => {
+
+    let opcion
+    console.log("1 Sumar    2 Restar")
+    console.log("3 Multiplicacion    4 Division")
+    console.log("5 Modulo    6 Salir")
+
+
+
+    do {
+        opcion =
+            parseInt(await leerTeclado('Elija una opcion'))
+        switch (opcion) {
+            case 1:
+                console.log("Primera opcion")
+                break
+            case 2:
+                console.log("Segunda opción")
+                break
+            case 3:
+                console.log("adios")
+                break
+        }
+
+    } while (opcion != 0)
+}
