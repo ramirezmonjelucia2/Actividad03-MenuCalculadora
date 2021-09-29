3e9df27da0b20c3ff7478725e82335237fb9ba3d
Ramirez Monje Lucia <lrammon392@g.educaand.es>
2021-09-29 09:10:58 +0200

    first commit

--- src/index2.ts
+++ src/index2.ts
@@ -1,18 +1,28 @@
 import { leerTeclado } from './util/entradaTeclado'
 
+let sumar = async () => {
+
+    let num1: number, num2: number, resultado: number
+
+    num1 = parseInt(await leerTeclado("Numero 1"))
+    num2 = parseInt(await leerTeclado("Numero 2"))
+    resultado = num1 + num2
+    console.log(`El resultado es: ${resultado}`)
+}
+
 let main = async () => {
 
     let opcion
 
     do {
-        console.log("1 Sumar             2 Restar")
-        console.log("3 Multiplicacion    4 Division")
-        console.log("5 Modulo            6 Salir")
-        opcion =
-            parseInt(await leerTeclado('Elija una opcion'))
+        console.log("1 Sumar             \n2 Restar")
+        console.log("3 Multiplicacion    \n4 Division")
+        console.log("5 Modulo            \n6 Salir")
+
+        opcion = parseInt(await leerTeclado('Elija una opcion'))
         switch (opcion) {
             case 1:
-                console.log("Primera opcion")
+                await sumar()
                 break
             case 2:
                 console.log("Segunda opción")
